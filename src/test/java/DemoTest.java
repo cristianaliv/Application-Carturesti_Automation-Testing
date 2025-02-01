08323cad4bfbfd1dd11f5226bdf3c77518cdeb53
crist <cristiana.livadariu@gmail.com>
2025-02-01 12:05:50 +0200

    -added selenium, junit,log4j and demo test

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,29 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.automation.framework.BrowserManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoTest {
+
+    BrowserManager manager = new BrowserManager();
+
+    Logger log = LogManager.getRootLogger();
+
+    @Test
+
+    public void browserTest() {
+        manager.openBrowser();
+        log.info("Browser opened");
+        WebDriver driver = manager.getDriver();
+        log.warn("this is a warning message");
+        driver.get("https://carturesti.ro/");
+
+        String url = driver.getCurrentUrl();
+        assertTrue(url.contains("carturesti"));
+
+    }
+
+}
